64a7084374f9fec3ac210479e705499e25514fbf
jkhb <backend_@kakao.com>
2021-04-12 14:56:49 +0900

    im adding login function

--- routes/users.ts
+++ routes/users.ts
@@ -110,9 +110,15 @@ router.post('/login', (req: express.Request, res: express.Response) => {
         'u_pwd': req.body.u_pwd
     };
     // TODO : 이메일, 비밀번호 조회 후 req.body와 DB상의 데이터 일치하는지 파악 후 에러뱉을지 200뱉을지
-    const queryForLogIn: string = connection.query('SELECT u_email from tbl_user WHERE u_id = ?', emailDataForLogIn,
+    const queryForLogIn: string = connection.query('SELECT u_email from tbl_user WHERE u_email = ?', emailDataForLogIn,
     function (err, result) {
         console.log(result);
+        if (result[0]===undefined) {
+            res.status(400).json({ error: '400', message: 'wrong email'});
+        }
+        else {
+            res.status(200).send({ message: 'login success'});
+        }
     })
 })
 
