a3d9dcf5723e4a2016ef2d0353d86127a5b27783
HAN-B-JK <backend_@kakao.com>
2021-09-13 00:48:53 +0900

    TODO \/ finish signUp & logIn

--- routes/index.ts
+++ routes/index.ts
@@ -7,4 +7,8 @@ router.get('/', (req: express.Request, res: express.Response) => {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/signUp', (req: express.Request, res: express.Response) => {
+    res.render('signUp', { title: 'Express' });
+});
+
 export = router;
